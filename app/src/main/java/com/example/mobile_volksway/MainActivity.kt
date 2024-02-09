14c5b464c8d676992af5d207cc2f6f0845e58b7d
Regina De Aguiar Becker Rosa <beckerrosakkph6@gmail.com>
2024-02-09 18:44:45 -0300

    atualizso

--- app/src/main/java/com/example/mobile_volksway/MainActivity.kt
+++ app/src/main/java/com/example/mobile_volksway/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.mobile_volksway
 
 import android.os.Bundle
+import android.widget.CheckBox
+import android.widget.Toast
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -12,18 +14,20 @@ import com.example.mobile_volksway.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var myCheckBox: CheckBox
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        myCheckBox = binding.checkBox
+
+        setupCheckBoxListener()
 
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
@@ -32,4 +36,10 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
-}
+
+    private fun setupCheckBoxListener() {
+        myCheckBox.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                Toast.makeText(this@MainActivity, "Caixa de seleção marcada", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeTe
