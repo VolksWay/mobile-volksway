8bef5798d68ffefbbb37e72403d5b440c9b7f128
Regina De Aguiar Becker Rosa <beckerrosakkph6@gmail.com>
2024-02-19 20:19:12 -0300

    escolha seu perfil

--- app/src/main/java/com/example/mobile_volksway/MainActivity.kt
+++ app/src/main/java/com/example/mobile_volksway/MainActivity.kt
@@ -72,7 +72,7 @@ class MainActivity : AppCompatActivity() {
 
 
     }
-
+-
 <<<<<<< HEAD
     private fun setupCheckBoxListener() {
         myCheckBox.setOnCheckedChangeListener { buttonView, isChecked ->
