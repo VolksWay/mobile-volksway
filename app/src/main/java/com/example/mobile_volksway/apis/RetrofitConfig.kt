587df7461500395d37f37ac9cfa955b30df8e72d
GustavoHenriqueFerreira <gustavohferreiraalves@gmail.com>
2024-02-21 21:38:06 -0300

    feat: Adicionando ajusts finais

--- app/src/main/java/com/example/mobile_volksway/apis/RetrofitConfig.kt
+++ app/src/main/java/com/example/mobile_volksway/apis/RetrofitConfig.kt
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 class RetrofitConfig {
     companion object{
 
-        fun obterInstanciaRetrofit(url: String = "http://172.16.52.81:8099/") : Retrofit{
+        fun obterInstanciaRetrofit(url: String = "http://172.16.52.167:8099/") : Retrofit{
 
             return Retrofit.Builder()
                 .baseUrl(url)
